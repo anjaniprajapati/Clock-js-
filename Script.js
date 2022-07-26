b4f5639c51413793d2234b1891a1858756755134
anjaniprajapati <91319771+anjaniprajapati@users.noreply.github.com>
2022-07-27 01:07:57 +0530

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,23 @@
+const hourHand=document.querySelector('#date-hour-hand')
+const minuteHand=document.querySelector('#date-minute-hand')
+const secondHand=document.querySelector('#date-second-hand')
+
+function setclock(secondHand, minuteHand,hourHand){
+    const currentDate=new Date();
+    const secondsRatio= currentDate.getSeconds()/60;
+    const minutesRatio= (secondsRatio+currentDate.getMinutes())/60;
+    const hoursRatio= (minutesRatio +currentDate.getHours())/12;
+    
+    
+    
+    setRotation(secondHand,secondsRatio);
+    setRotation(minuteHand,minutesRatio);
+    setRotation(hourHand,hoursRatio);      
+}
+
+
+function setRotation(element,rotationRatio){
+    element.style.setProperty('--rotation', rotationRatio*360); 
+    
+}
+setInterval(() => setclock(secondHand, minuteHand,hourHand),1000)
